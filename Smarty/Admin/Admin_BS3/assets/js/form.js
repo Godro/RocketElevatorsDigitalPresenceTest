04890b6f0ec69ce65c74a5d361fe236ba2d56ab0
Godro <69221381+Godro@users.noreply.github.com>
2020-09-11 12:01:04 -0400

    deuxieme push

---
+++ Smarty/Admin/Admin_BS3/assets/js/form.js
@@ -0,0 +1,40 @@
+
+
+
+ function change() {
+     
+        console.log("allo");
+    
+}
+
+
+
+/*function show(select){
+    if (select.value==0){
+        
+        document.getElementById("res-details").style.display = "none";
+        document.getElementsById("com-details").style.display = "none";
+        document.getElementsById("cor-details").style.display = "none";
+        document.getElementsById("hyb-details").style.display = "none";
+       }
+        if (select.value==1){
+            document.getElementById("res-details").style.display = "block";
+           } else {
+            document.getElementsById("res-details").style.display = "none";
+           } 
+        if (select.value==2) {
+            document.getElementById("com-details").style.display = "block";
+        } else {
+         document.getElementsById("com-details").style.display = "none";
+        }   
+        if (select.value==3) {
+            document.getElementById("cor-details").style.display = "block";
+        } else {
+         document.getElementsById("cor-details").style.display = "none";
+        } 
+        if (select.value==4) {
+            document.getElementById("hyb-details").style.display = "block";
+        } else {
+         document.getElementsById("hyb-details").style.display = "none";
+        }     
+} */
